561ce6803e361d2f85fa0640a9ea5840c4dd4498
Ruben Nobre <ruben.nobre@indiecampers.com>
2025-06-03 11:19:21 +0100

    add due date field to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -41,6 +41,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :description, :complete)
+    params.require(:task).permit(:title, :description, :complete, :due_date)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20250601142500) do
+ActiveRecord::Schema.define(version: 20250603100857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20250601142500) do
     t.integer  "user_id"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.date     "due_date"
   end
 
   add_index "tasks", ["user_id"], name: "index_tasks_on_user_id", using: :btree
